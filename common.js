6c427a10b558ce4e29cb81481938e2372db269fb
Ravi Dikshit <dixitravi@gmail.com>
2026-05-06 19:37:53 +0530

    Update common.js

--- common.js
+++ common.js
@@ -24,12 +24,14 @@ async function loadContent(isAdmin = false) {
       data.experience.forEach(item => {
         const div = document.createElement("div");
         div.className = "timeline-item";
-        div.innerHTML = `
-          <h3>${item.role}</h3>
-          <strong>${item.org}</strong>
-          <div>${item.duration}</div>
-          <p>${item.desc}</p>
-        `;
+
+div.innerHTML = `
+  <h3>${item.role}</h3>
+  <strong>${item.org}</strong>
+  <div>${item.duration}</div>
+  <p>${item.desc}</p>
+`;
+
         timeline.appendChild(div);
       });
     }
