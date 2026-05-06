8eadf763548154efb5811ab232a296c080bd58be
Ravi Dikshit <dixitravi@gmail.com>
2026-05-06 18:00:23 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,13 +1,23 @@
-let currentData = null;let currentData = data;
-  document.getElementById("siteTitleInput").value = data.site.title;
-  document.getElementById("colorInput").value = data.site.primaryColor;
-  document.getElementById("heroHeadingInput").value = data.hero.heading;
+const pass = prompt("Enter admin password");const pass = prompt("Enter admin !== "test@33") {
+  alert("Access denied");
+  document.body.innerHTML = "";
 }
 
-async function save() {
+
+
+
+let currentData = null;let currentData = nulllineInput").value = data.site.tagline;
+  document.getElementById("heroHeadingInput").value = data.hero.heading;
+  document.getElementById("heroSubtextInput").value = data.hero.subtext;
+  document.getElementById("aboutContentInput").value = data.about.content;
+}
+
+async function saveContent() {
   currentData.site.title = document.getElementById("siteTitleInput").value;
-  currentData.site.primaryColor = document.getElementById("colorInput").value;
+  currentData.site.tagline = document.getElementById("siteTaglineInput").value;
   currentData.hero.heading = document.getElementById("heroHeadingInput").value;
+  currentData.hero.subtext = document.getElementById("heroSubtextInput").value;
+  currentData.about.content = document.getElementById("aboutContentInput").value;
 
   await fetch("/api/content", {
     method: "POST",
@@ -15,7 +25,10 @@ async function save() {
     body: JSON.stringify(currentData)
   });
 
-  alert("Content updated successfully ✅");
+  alert("✅ Website updated successfully");
 }
 
 function fillForm(data) {
+  currentData = data;
+
+  document.getElementById("siteTitleInput").value = data.site.title;
